fe5859bbd45f590b9d58060e6a96f7cc263f74b4
alexia <alexiaedu012004@gmail.com>
2022-08-18 20:35:59 -0300

    doin

--- PROGRAMAS/PHP/cadastroImagens.php
+++ PROGRAMAS/PHP/cadastroImagens.php
@@ -1,29 +1,51 @@
 <?php
-include('conexao.php');
 
-if (isset($_FILES['imagens'])){
-    $arquivo = $_FILES['imagens'];
-    $pasta = 'imagens/';
+include ('conexao.php');
 
-    if($arquivo['error']){
-        die("falha ao enviar imagem");}
+function enviarImagem($error, $size, $name, $tmp_name) {
+    include ('conexao.php');
 
-    $nomeImagem = $arquivo['name'];
+    if($error){
+    die("falha ao enviar arquivo");
+    }
+    if($size > 2097152)
+        die('arquivo muito grande!! Max: 2MB');
+    
+    $pasta = 'past_arquivos/';
+    $nomeImagem = $name;
+    $novoNomeImagem = uniqid();
     $extensao = strtolower(pathinfo($nomeImagem, PATHINFO_EXTENSION));
     
-    if($extensao != 'jpg' && $extensao != 'jpeg' && $extensao != 'png'){
+    if($extensao != 'jpg' && $extensao != 'jpeg' && $extensao != 'png')
         die("Tipo de arquivo não aceito");
-    }
-
-    $path = $pasta . $nomeImagem. "." . $extensao;
-    $deu_certo = move_uploaded_file($arquivo["tmp_name"], $path);
+    
+    
+    $path = $pasta . $novoNomeImagem. "." . $extensao;
+    $deu_certo = move_uploaded_file($tmp_name, $path);
 
     if($deu_certo){
     $con->query("INSERT INTO Imagem (imagem_name, imagem_arq) VALUES('$nomeImagem','$path')") or die ($con->error);
-     //echo "<p>Arquivo enviado com sucesso! para acessa-lo, clique aqui:<a href = \"imagens/$nomeImagem.$extensao\">clique aqui</a></p>";
+     return true;
     }
-     else
-        echo "<p>Falha ao enviar arquivo</p>";
+     else {
+        return false;
+     }
+}
+
+if (isset($_FILES['imagens'])){
+    $imagens = $_FILES['imagens'];
+    $tudo_certo= true;
+    foreach($imagens['name'] as $arq => $img) {
+        $deu_certo = enviarImagem($imagens['error'][$arq], $imagens['size'][$arq], $imagens['name'][$arq],$imagens["tmp_name"][$arq]);
+        if(!$deu_certo)
+            $tudo_certo = false;
+    }
+
+    if($tudo_certo)
+        echo "todos os aquivos foram enviados";
+    
+    else 
+        echo "falha no envio dos arquivos";
 }
 ?>
 <!DOCTYPE html> 
@@ -37,7 +59,7 @@ if (isset($_FILES['imagens'])){
 <body> 
     <form method = "post" action ="" enctype="multipart/form-data"> 
         <label for="imagem">Imagem: </label> 
-        <input multiple type="file" name="imagens" id="imagem">
+        <input multiple="multiple" type="file" name="imagens[]">
         <input type="submit" value="Submit">
 </form>
 </body>
